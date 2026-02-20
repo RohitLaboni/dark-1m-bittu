89e04528aff53daec473c9556ce8ccfb6a76761c
RohitLaboni <151800110+RohitLaboni@users.noreply.github.com>
2026-02-20 22:08:02 +0530

    Update display name in ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -15,7 +15,7 @@ Module({
     },
     message: {
       contactMessage: {
-        displayName: "𝑴𝒓 𝑹𝒂𝒃𝒃𝒊𝒕'ʬ 合",
+        displayName: "𝑴𝒓 BITTU'ʬ 合",
         vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:'DEMON'\nitem1.TEL;waid=${
           message.conn.user.id.split("@")[0]
         }:${
